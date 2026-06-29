80977a05ae3cb0fc60698572c466cbeb46f78af4
vigneshtsv <vigneshtsv27@gmail.com>
2026-06-29 14:01:24 +0530

    updated

--- Database/dataBase.js
+++ Database/dataBase.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -9,7 +9,29 @@ const connectDB = async (req,res) => {
         console.log("MongoDB Connected Successfully");
         return connection;
     } catch (error) {
-        console.error("MongoDB Connection Error");
+        console.error("MongoDB Connection Error:", error.message);
+        console.log(process.env.MONGODB_URL);
     }
 };
 export default connectDB;
+
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const connectDB = async () => {
+//     try {
+//         const connection = await mongoose.connect(process.env.MONGODB_URL);
+
+//         console.log("MongoDB Connected Successfully");
+//         console.log(`Host: ${connection.connection.host}`);
+
+//     } catch (error) {
+//         console.error("MongoDB Connection Error:", error.message);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
